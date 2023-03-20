99e5f502a22261cab5eda98996dd50c220dc2a20
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-20 03:05:02 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 /**
  *  main -Entry point
  *
  *  Return: Always 0 (Success)
  */
-#include <stdio.h>
-int main()
+int main(void)
 {
-puts(""Programming is like building a multilingual puzzle\n");
-return(0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
