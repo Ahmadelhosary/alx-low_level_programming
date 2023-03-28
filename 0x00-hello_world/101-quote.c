a8a181d7b81958e1e5e3b746c6e8058a58b2a033
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-28 08:49:05 +0200

    created 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main()
+{
+write(1,"and that piece of art is useful\"- Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\"- Dora Korpar, 2015-10-19"));return 1;
+}
