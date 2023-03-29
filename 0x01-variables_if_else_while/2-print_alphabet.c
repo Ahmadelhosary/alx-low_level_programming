37194968429c756ca8cbd5b44f85e870f59ebe0c
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-29 06:36:19 +0200

    created print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - prints alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int i = 97;
+		while (i <= 122)
+		{
+			putchar(i);
+			i++;
+		}
+	return 0;
+}
+
