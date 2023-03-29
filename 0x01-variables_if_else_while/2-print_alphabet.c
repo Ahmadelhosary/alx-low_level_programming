742a2f2973e92ba43dfb62c1c283fafde6cafaef
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-03-29 22:55:17 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main -> prints lowercase  alphabet using putchar
+/**
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
 int main(void)
