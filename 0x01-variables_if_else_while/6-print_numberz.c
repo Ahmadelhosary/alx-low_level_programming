9455e43699d634fc7af01b2832c0af765bc44488
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-30 04:08:22 +0200

    created 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i = 48;
+
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+return (0);
+}
