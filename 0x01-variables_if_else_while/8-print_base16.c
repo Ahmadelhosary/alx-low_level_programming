2768fd3715f58a1378a12c0c9b303ed08ce4deeb
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-30 04:59:30 +0200

    created 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i = 48;
+int x = 97;
+
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+
+while (x <= 102)
+{
+putchar(x);
+x++;
+}
+
+putchar(10);
+return (0);
+}
