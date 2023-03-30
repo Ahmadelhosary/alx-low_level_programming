9b342596c4ba958518bcb63d3a69f726ba162597
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-30 02:05:35 +0200

    fix 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet in lowercase except e,q.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
@@ -8,16 +8,15 @@
 int main(void)
 {
 int i = 97;
-int error;
-while (i <= 122 && error == 0)
+int error = 0;
+while (i <= 122)
 {
-putchar(i);
 if (i == 101 || i == 113)
 {
-error = 1;
-}
 i++;
-error = 0;
+}
+putchar(i);
+i++;
 }
 putchar(10);
 return (0);
