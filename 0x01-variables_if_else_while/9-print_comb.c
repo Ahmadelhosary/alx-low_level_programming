1065d280049991d9f79c7ca5cc42480a3d770336
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-03-30 06:06:00 +0200

    create-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i = 48;
+int space = 32;
+int comma = 44;
+
+while (i <= 57)
+{
+putchar(i);
+if (i < 57)
+putchar(comma);
+if (i < 57)
+putchar(space);
+
+i++;
+}
+return (0);
+}
