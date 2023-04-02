66870284c253df8918e8de2a5372a1ecfee0dbfa
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-04-02 08:00:28 +0200

    added 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l++;
+
+		_putchar('\n');
+	}
+}
