eb73bb339a02e8a2deecb79f636985de8bc579b5
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-04-03 03:07:18 +0200

    added 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int_abs - Prints the abs of a number.
+ * @n: The number will be printed.
+ *
+ * Return: The same n if more or equal to zero,
+ *         -1 * n if the number is less than zero.
+ */
+int int_abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	
+
+	else
+	{
+		return (-1 * n);
+	}
+}
