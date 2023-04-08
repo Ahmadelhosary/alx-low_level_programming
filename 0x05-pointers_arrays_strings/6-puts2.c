1f1e23a211be379c87eec5913953177b9856d64a
Ahmadelhosary <ahmedmahmoud199940@gmail.com>
2023-04-08 12:16:22 +0200

    created 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * puts2 - write the other character of a string
+ * _putchar - print each character
+ * @str: char to check
+ *
+ * Description: This will reverse a string
+ * Return: 0 is success
+ */
+
+
+void puts2(char *str)
+{
+
+int i;
+
+i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+
+
+_putchar('\n');
+}
