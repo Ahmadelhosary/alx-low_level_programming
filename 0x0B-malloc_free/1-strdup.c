074386a560e14ed4326cea8b34f6cb4c96ad5558
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-04-13 08:44:03 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,18 +14,18 @@ char *_strdup(char *str)
 {
 	char *arr;
 	int x, len;
-	
+
 	len = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
-
+	
 	while (str[len] != '\0')
 		len++;
 
 
 	arr = (char *)malloc((sizeof(char) * len) + 1);
-	
+
 	if (arr == NULL)
 		return (NULL);
 	x = 0;
@@ -35,6 +35,5 @@ char *_strdup(char *str)
 		arr[len] = '\0';
 	}
 	return (arr);
-	free (arr);
+	free(arr);
 }
-
