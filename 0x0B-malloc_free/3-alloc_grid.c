5ae074058e0e79431d3bbb24d32288b8316e2445
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-04-14 20:14:11 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,42 +9,32 @@
  * Return: a pointer to a 2 dimensional array of integers.
  */
 
-int**	alloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 {
-	int**	g;
-	int		i, j;
+	int **g;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
-	{
-		return NULL;
-	}
+		return (NULL);
 
 	g = (int **)malloc(sizeof(int *) * height);
-
 	if (g == NULL)
-	{
-		return NULL;
-	}
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
 		g[i] = (int *)malloc(sizeof(int) * width);
-
 		if (g[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-			{
 				free(g[j]);
-			}
 			free(g);
-			return NULL;
+			return (NULL);
 		}
 
 		for (j = 0; j < width; j++)
-		{
 			g[i][j] = 0;
-		}
 	}
 
-	return g;
+	return (g);
 }
