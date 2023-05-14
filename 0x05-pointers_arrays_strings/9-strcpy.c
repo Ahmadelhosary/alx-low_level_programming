d2423ab098576df173fc5dc7a03c60fe54d92bcd
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-05-14 14:49:26 +0300

    created 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - copy string
+ *@dest: array we copy to it
+ *@src:array we copy from it
+ *
+ *Return: dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+
+
+}
