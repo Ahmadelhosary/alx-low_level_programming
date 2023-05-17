3fca4c9d88218436974b8b95a8083e8de3062311
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-05-17 20:45:18 +0300

    created 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+char *ss = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (sizeof(src) == n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ss);
+
+}
