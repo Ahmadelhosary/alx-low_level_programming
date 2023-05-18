370e4acd95144b15a3ef28f268de0693480c3800
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-05-18 15:36:47 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,16 +14,16 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 < *s2)
 		{
-		return -1;
+		return (-1);
 		}
 		else if (*s1 > *s2)
 		{
-		return 1;
+		return (1);
 		}
 
 		s1++;
 		s2++;
 		}
 
-	return 0;
+	return (0);
 }
