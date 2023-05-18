6165c8915a41c26c91b4b27b94f1ef97a916ae73
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-05-18 14:45:30 +0300

    created 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 string lexicographically
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+while (*s1 != '\0' || *s2 != '\0')
+{
+if (*s1 > *s2)
+return 1;
+if (*s1 < *s2)
+return -1;
+if (*s1 < *s2)
+return -1;
+if (*s1 == *s2)
+return 0;
+}
+*s1++;
+*s2++;
+}
