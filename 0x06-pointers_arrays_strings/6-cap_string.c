fa2b2a3d7212f174d108e23054a955875e7488bc
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-05-20 16:43:34 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes every word of a string
- * @c: string to modify
+ * cap_string - Capitalizes the first letter of each word in a string
+ * @str: The string to be capitalized
  *
- * Return: the resulting string
+ * Return: Pointer to the capitalized string
  */
-
-char *cap_string(char *c)
+char *cap_string(char *str)
 {
-int i;
+	int i = 0;
 
-i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
 
-for (i = 0; c[i] != '\0'; i++)
-{
-if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == ',' || c[i] == ';' || c[i] == '.'
-	|| c[i] == '!' || c[i] == '?' || c[i] == '"' || c[i] == '(' || c[i] == ')'
-	|| c[i] == '{' || c[i] == '}')
-{
-if (c[i] >= 97 && c[i] <= 122)
-c[i] -= 32;
-}
-}
-return (c);
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
+		i++;
+	}
 
+	return str;
 }
