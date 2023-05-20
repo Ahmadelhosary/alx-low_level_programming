4d71e15d3e952e45105579f5e049d8bd3503e167
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-05-20 15:27:00 +0300

    fix 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes every word of a string
+ * @c: string to modify
+ *
+ * Return: the resulting string
+ */
+
+char *cap_string(char *c)
+{
+int i;
+
+i = 0;
+
+for (i = 0; c[i] != '\0'; i++)
+{
+if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == ',' || c[i] ==
+|| c[i] == '.' || c[i] == '!' || c[i] == '?' || c[i] == '"'
+|| c[i] == '(' || c[i] == ')' || c[i] == '{' || c[i] == '}')
+{
+if (c[i] >= 97 && c[i] <= 122)
+c[i] -= 32;
+}
+}
+return (c);
+
+}
