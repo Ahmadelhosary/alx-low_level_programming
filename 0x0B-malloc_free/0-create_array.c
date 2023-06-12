1010bb0b704e0dd9be4f623453b020e80998e845
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-12 17:30:41 +0300

    fix create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,22 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i = 0;
 
 	if (size == 0)
-	return (NULL);
+		return (NULL);
 
 	str = (char *)malloc(size * sizeof(char));
 
-	if (str != NULL)
-	*(str + 0) = c;
+	if (str == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		*(str + i) = c;
+		i++;
+	}
+	*(str + i) = '\0';
 
 	return (str);
 }
-
