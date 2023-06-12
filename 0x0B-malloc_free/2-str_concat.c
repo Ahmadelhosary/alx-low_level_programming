a0c3f83aa5da7440a792ce50dfa2c15475c075f0
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-12 21:18:10 +0300

    cr strcat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *co;
+int i, j, v, z;
+int l1, l2;
+
+if (s1 == NULL || s2 == NULL)
+return (NULL);
+
+l1 = 0;
+l2 = 0;
+while (s1[l1] != '\0')
+l1++;
+
+while (s2[l2] != '\0')
+l2++;
+
+co = (char *)malloc(((l1 + l2)+2) * sizeof(char));
+
+if (co == NULL)
+return (NULL);
+
+i = 0;
+j = 0;
+while (i < l1)
+{
+co[i] = s1[j];
+i++;
+j++;
+}
+z = l1;
+v = 0;
+while (z < (l1 + l2))
+{
+co[z] = s2[v];
+z++;
+v++;
+}
+co[z] = '\0';
+return (co);
+}
