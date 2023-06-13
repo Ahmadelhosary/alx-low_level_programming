a81491c2d89a22bc685a1aad7b4e092acb54b69a
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-13 16:44:01 +0300

    cr grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - creates a two dimensional array of ints
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: pointer to the created matrix (Success)
+ * or NULL (Error)
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **arr;
+int i, j;
+
+if (width <= 0 || height <= 0)
+	return (NULL);
+
+arr = (int **)malloc(height * sizeof(int *));
+
+for (i = 0; i < height; i++)
+{
+arr[i] = (int *)malloc(width * sizeof(int));
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+if (arr == NULL)
+	return (NULL);
+
+return (arr);
+    
+}
