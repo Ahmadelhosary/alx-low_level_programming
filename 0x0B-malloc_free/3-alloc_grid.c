9d9b20917ab8028c2f2b0f07cfa9ac6420c84eca
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-13 18:28:33 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,12 +24,14 @@ for (i = 0; i < height; i++)
 {
 arr[i] = (int *)malloc(width * sizeof(int));
 	if (arr[i] == NULL)
+	{
 	for (j = 0; j <= i; j++)
 	{
 		free(arr[j]);
 	}
 	free(arr);
 	return (NULL);
+	}
 for (j = 0; j < width; j++)
 {
 arr[i][j] = 0;
