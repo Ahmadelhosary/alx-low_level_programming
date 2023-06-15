d34ae90cb856a634dc95df18cba510964757e777
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-15 23:14:59 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,12 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *co;
 unsigned int i, j, v, z, l1, l2;
 
-i = 0; j = 0; z = l1; v = 0;
+i = 0, j = 0, v = 0, l1 = 0, l2 = 0;
+z = l1
 if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
-l1 = 0; l2 = 0;
+
 while (s1[l1] != '\0')
 l1++;
 while (s2[l2] != '\0')
@@ -33,21 +34,15 @@ if (co == NULL)
 return (NULL);
 while (i < l1)
 {
-co[i] = s1[j];
-i++;
-j++;
+co[i++] = s1[j++];
 }
 while (n < l2 && z < (l1 + n))
 {
-co[z] = s2[v];
-z++;
-v++;
+co[z++] = s2[v++];
 }
 while (n >= l2 && z < (l1 + l2))
 {
-co[z] = s2[v];
-z++;
-v++;
+co[z++] = s2[v++];
 }
 co[z] = '\0';
 return (co);
