6a9bde909b2a0edae53d93067ad9238f2628c624
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-15 15:47:09 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * @b - size of allocation
+ * @b: size of allocation
  * @*malloc_checked - takes size and return a pointer to allocated memory
  * Return: pointer to allocated space or 98 if failed
  */
