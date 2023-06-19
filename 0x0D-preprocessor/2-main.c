04c6241e25a136a80626f63d1e7c10b17aebf9b3
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-19 20:19:56 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-#define PRINT_FILE_NAME printf("File name: %s\n", __FILE__)
-
 /**
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-PRINT_FILE_NAME;
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
