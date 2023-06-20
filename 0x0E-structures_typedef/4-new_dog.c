f9dae994c07c0b8be1b47f635563d0dd0ec93072
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-20 18:32:18 +0300

    cr 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+
+
+/**
+ * new_dog - creates a new dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the newly created dog structure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *ndog;
+int l1, l2, i, j;
+
+
+ndog = (dog_t *)malloc(sizeof(dog_t));
+
+if (ndog == NULL)
+return (NULL);
+
+while (name[l1] != '\0')
+l1++;
+while (owner[l2] != '\0')
+l2++;
+
+ndog->name = (char *)malloc((l1 + 1) * sizeof(char));
+ndog->owner = (char *)malloc((l2 + 1) * sizeof(char));
+
+for (i = 0; i < l1; i++)
+ndog->name[i] = name[i];
+ndog->name[i] = '\0';
+
+for (j = 0; j < l2; j++)
+ndog->owner[j] = owner[j];
+ndog->owner[j] = '\0';
+
+ndog->age = age;
+
+return (ndog);
+}
