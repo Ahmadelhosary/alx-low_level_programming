0be39cd7349d4359f5fc79a6ce1f8013aa811d12
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-21 22:34:15 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, j;
 
-if (size == 0)
-return (-1);
+	if (size == 0)
+		return (-1);
 
 for (i = 0; i < size; i++, j++)
 {
@@ -25,7 +25,6 @@ cmp(array[i]);
 j = cmp(array[i]);
 if (j != 0)
 return (i);
-else
-return (-1);
 }
+	return (-1);
 }
