f6a23f77dbdae950ea401a20fe3ea0e17b5d1ed7
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-21 22:18:20 +0300

    fix 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - earches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, j;
+
+if (size == 0)
+return (-1);
+
+for (i = 0; i < size; i++, j++)
+{
+cmp(array[i]);
+j = cmp(array[i]);
+if (j != 0)
+return (i);
+else
+return (-1);
+}
+}
