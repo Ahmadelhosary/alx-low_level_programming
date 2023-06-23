7e5a8163e6e448a8985e2081b2b40b44e36ba42a
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-23 17:56:12 +0300

    cr 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+const char *x;
+
+va_start(args, n);
+
+for (i = 0; i < n; ++i)
+{
+if (i > 0 && separator != NULL)
+	printf("%s", separator);
+
+x = va_arg(args, const char*);
+if (!x)
+	printf("nil");
+else
+	printf("%s", x);
+}
+printf("\n");
+va_end(args);
+}
