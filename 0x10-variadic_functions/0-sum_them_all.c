59901c9549156c21300ea0d05a6474f25e3aafef
Ahmadelhosary <ahmedmahmoud199940@gmail.comgit config --global user.name Ahmadelhosary>
2023-06-23 10:51:38 +0300

    cr 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - summate all arguments
+ * @n: number of arguments
+ *
+ * Return: the sum
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int x, sum;
+x = 0, sum = 0;
+va_list args;
+
+va_start(args, n);
+
+for (i = 0; i < n; ++i)
+{
+x = va_arg(args, int);
+sum += x;
+}
+
+va_end(args);
+
+return (sum);
+}
