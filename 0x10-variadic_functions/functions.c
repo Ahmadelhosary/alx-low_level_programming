9be3414b5a446c9625710ce01c7b5d592a370635
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-24 12:20:51 +0300

    Update functions.c

--- 0x10-variadic_functions/functions.c
+++ 0x10-variadic_functions/functions.c
@@ -1,1 +1,45 @@
+#include "variadic_functions.h"
 
+/**
+ * print_c - Print a character from va_list
+ * @args: va_list containing the character to print
+ * This function retrieves a character argument from va_list and prints it.
+ */
+void print_c(va_list args)
+{
+int c = va_arg(args, int);
+printf("%c", c);
+}
+
+/**
+ * print_i - Print an integer from va_list
+ * @args: va_list containing the integer to print
+ * This function retrieves an integer argument from va_list and prints it.
+ */
+void print_i(va_list args)
+{
+int i = va_arg(args, int);
+printf("%d", i);
+}
+
+/**
+ * print_f - Print a floating-point number from va_list
+ * @args: va_list containing the floating-point number to print
+ * This function retrieves a float argument va_list and prints it.
+ */
+void print_f(va_list args)
+{
+double f = va_arg(args, double);
+printf("%f", f);
+}
+
+/**
+ * print_s - Print a string from va_list
+ * @args: va_list containing the string to print
+ * This function retrieves a string argument from va_list and prints it.
+ */
+void print_s(va_list args)
+{
+char *s = va_arg(args, char*);
+printf("%s", s);
+}
