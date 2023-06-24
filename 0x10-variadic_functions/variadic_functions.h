06b6dae549aa6bd2d7ebb127db59161484c52179
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-06-24 12:32:46 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,27 +4,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * struct operator - choose
- * @op: First member which string
- * @f: Second member function
- *
- * Description: Longer description
- */
-struct operator
-{
-const char *op;
-void (*f)(va_list);
-};
-typedef struct operator op_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_c(va_list args);
-void print_i(va_list args);
-void print_f(va_list args);
-void print_s(va_list args);
 
 #endif
