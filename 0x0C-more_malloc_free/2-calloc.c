fc3f48d8d8fddf3deb147d47beb1c65c48d34101
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-04 22:40:49 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,13 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *arr;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
-		return NULL;
+		return (NULL);
 
 	memset(arr, 0, nmemb * size);
 
-	return arr;
+	return (arr);
 }
