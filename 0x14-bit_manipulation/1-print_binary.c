33f8a128e3247f9e208ea09deb619b9bcf759623
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-08 16:17:10 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -24,5 +24,5 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);  /* Right shift by 1 bit */
-_putchar((n & 1) ? '1' : '0');  /* Print the least significant bit */ 
+_putchar((n & 1) ? '1' : '0');  /* Print the least significant bit */
 }
