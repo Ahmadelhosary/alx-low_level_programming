63a7fdf813da1e1c5658388ce77f4a80321a3ff8
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-08 16:25:56 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,5 @@
 #include "main.h"
 
-/**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
- *
- * Return: On success, returns the number of bytes written.
- *         On error, returns -1.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 
 /**
  * print_binary - Prints the binary representation of a number.
