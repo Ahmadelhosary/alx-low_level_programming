04b322cd0ee979c2686f6a5a5aa16b5e6d3340f1
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-09 12:42:45 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,16 +9,17 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+		unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		/* Error: Index is out of bounds */
 		return (-1);
 	}
-	unsigned long int mask;
+
 
 	mask = 1UL << index;
 	*n |= mask;
 
 	return (1);
 }
-
