ae5e20928492680b6710937b7474035fd9ee2170
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-09 13:02:29 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+
 
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
@@ -17,8 +17,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	}
 
-	mask = ~(1UL << index);
-	*n &= mask;
+
+	*n &= (~(1UL << index));
 
 	return (1);
 }
