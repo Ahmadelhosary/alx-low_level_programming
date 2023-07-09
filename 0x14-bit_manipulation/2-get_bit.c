c5c6c7f48c59d1c9ea803fe683bbca37a06cf9a7
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-07-09 13:29:36 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,18 +2,24 @@
 
 /**
  * get_bit - Returns the value of a bit at a given index.
- * @n: The unsigned long int number to extract the bit from.
- * @index: The index of the bit to retrieve.
+ * @n: The unsigned long int number.
+ * @index: The index of the bit to be retrieved.
  *
- * Return: The value of the bit at the given index, or -1 if an error occurs.
+ * Return: The value of the bit at the given index or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
-	unsigned long int m = 1 << index;
+	unsigned long int mask;
+	unsigned long int result;
 
 	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	{
+		/* Error: Index is out of bounds */
+		return -1;
+	}
 
-	return ((n & m) ? 1 : 0);
+	mask = 1UL << index;
+	result = n & mask;
+
+	return (result != 0);
 }
